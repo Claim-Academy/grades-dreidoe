2deff92722563cade22c81d52b78cf024c82d328
Aundrei Bryant <drei97@gmail.com>
2023-01-12 10:55:24 -0600

    started: test for function

--- app/lib.test.js
+++ app/lib.test.js
@@ -1,31 +1,41 @@
 import { getStudentGradeById } from "./lib";
 
-it("should find a specific grade for a specific student",() => {
-const inputRoster = [
-  {
-    id: 1,
-    grades: [
-      { id: 1},
-      { id: 2},
-      { id: 3},
-    ]
-  },
-  {
-    id: 2,
-    grades: [
-      { id: 1},
-      { id: 2},
-      { id: 3},
-    ]
-  }
-];
-const inputGradeId = 2;
-const inputStudentId = 1;
-const expected = { id:2 };
+it("should find a specific grade for a specific student", () => {
+  const inputRoster = [
+    {
+      id: 1,
+      grades: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    },
+    {
+      id: 2,
+      grades: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    },
+  ];
+  const inputGradeId = 2;
+  const inputStudentId = 1;
+  const expected = { id: 2 };
 
-const actual = getStudentGradeById ({
-  roster: inputRoster,
-  gradeId: inputGradeId,
-  student: inputStudentId,
+  const actual = getStudentGradeById({
+    roster: inputRoster,
+    gradeId: inputGradeId,
+    student: inputStudentId,
+  });
+  expected(actual).toEqual(expected);
 });
-expected (actual).toEqual(expected);
+
+it("should find a specific grade for a specific student", () => {
+  const inputRoster = [
+    {
+      id: 1,
+      grades:
+      [ { id: 1, type: "HW" },
+        { id: 2  type: "Test"},
+        { id: 3 type: "HW"},
+      ],
+    },
+    {
+      id: 2,
+      grades: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    },
+  ];
+
