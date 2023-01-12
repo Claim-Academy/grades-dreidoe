2db2422dc5b131eecd90aa9ceb899ac38698efb2
Aundrei Bryant <drei97@gmail.com>
2023-01-12 09:44:28 -0600

    created : libs functions

--- app/lib.js
+++ app/lib.js
@@ -1,3 +1,11 @@
 import apiService from "./api.service.js";
 
 const grades = await apiService.getGrades();
+
+export const getAllNamesFromRoster = (roster) =>
+  roster.map((person) => person.name);
+
+export const getStudentById = (roster, id2Find) =>
+  roster.find((person) => person.id === id2Find);
+
+export const getStudenGradeById = (gradeID, studentId) => {};
